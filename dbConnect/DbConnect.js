76d0ec0e2d8d5b3bf3e5ed263fecec7437e1935c
Ravi Tiwari <raviashoktiwari9559@gmail.com>
2025-12-09 15:03:45 +0530

    mongoose global conn setup

--- dbConnect/DbConnect.js
+++ dbConnect/DbConnect.js
@@ -1,14 +1,36 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const dbConnect =async(req,res)=>{
-    try {
-        const db =await mongoose.connect(process.env.MONGODB_URL)
-        if(db){
-            console.log('MongoDb connected successfully..');
-        }
-    } catch (error) {
-        console.log(error)
-    }
+// const dbConnect =async(req,res)=>{
+//     try {
+//         const db =await mongoose.connect(process.env.MONGODB_URL)
+//         if(db){
+//             console.log('MongoDb connected successfully..');
+//         }
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// export {dbConnect}
+
+import mongoose from 'mongoose';
+
+
+if (!global.mongoose) {
+  global.mongoose = { conn: null, promise: null };
 }
 
-export {dbConnect}//here we connect the database to it
+async function dbConnect() {
+  if (global.mongoose.conn) {
+    return global.mongoose.conn;
+  }
+
+  if (!global.mongoose.promise) {
+    global.mongoose.promise = mongoose.connect(process.env.MONGODB_URL);
+  }
+
+  global.mongoose.conn = await global.mongoose.promise;
+  return global.mongoose.conn;
+}
+
+export {dbConnect};
